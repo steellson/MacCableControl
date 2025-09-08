7caf912a015ba0f6ad741c824f50ff0ef079e7ff
Andrew Steellson <113777921+steellson@users.noreply.github.com>
2025-09-08 17:58:44 +0400

    Initial Commit

---
+++ MacCableControl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacCableControl
+//
+//  Created by Andrew Steellson on 08.09.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
